570b454c9d1af4fe986b9ac604a561c2bbf61972
John Wesley Hostetter <jwhostet@ncsu.edu>
2022-11-27 00:16:59 -0500

    Extract abstract class from product inference engine; implement minimum inference

--- tests/fuzzy/logic/inference/test_engines.py
+++ tests/fuzzy/logic/inference/test_engines.py
@@ -3,9 +3,9 @@ import unittest
 
 from soft.fuzzy.sets import Gaussian
 from utils.reproducibility import set_rng
+from soft.fuzzy.logic.rules.knowledge import Rule
 from soft.fuzzy.information.granulation import GranulesMap
 from soft.fuzzy.logic.inference.engines import ProductInference
-from soft.fuzzy.logic.rules.knowledge import Rule, FuzzyRuleBase
 from soft.fuzzy.logic.control.tsk import make_links_from_antecedents_to_rules
 
 
