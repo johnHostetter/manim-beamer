95b2e0769084503a38e043e610b2e9adafe4b8f7
johnHostetter <jhostetter16@gmail.com>
2023-01-23 12:35:46 -0500

    Update supervised TSK example for new code

--- tests/fuzzy/information/test_granulation.py
+++ tests/fuzzy/information/test_granulation.py
@@ -6,9 +6,16 @@ from soft.fuzzy.information.granulation import GranulesGraph
 
 
 class TestGranulation(unittest.TestCase):
-    def test_input_granulation(self):
+    def test_add_edges(self):
         graph = GranulesGraph(granules=[Gaussian(8), Gaussian(4), Gaussian(6)])
+        # each tuple is (variable index, term index)
         graph.add(AlgebraicProduct, ((0, 1), (1, 1)), ((1, 2), (2, 3)), ((0, 7), (1, 3), (2, 5)))
-        # graph.add(AlgebraicProduct, [(0, 1)])
-        # map = GranulesMap(in_features=2, granules_params=[Gaussian(2), Gaussian(2)], membership_function=Gaussian)
-        graph.visualize()
+        expected_nodes = [
+            (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (1, 0), (1, 1), (1, 2), (1, 3),
+            (1, 4), (1, 5), (1, 6), (1, 7), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)
+        ]
+        expected_edges = [
+            ((0, 1), (1, 1)), ((0, 7), (1, 3)), ((0, 7), (2, 5)), ((1, 2), (2, 3)), ((1, 3), (2, 5))
+        ]
+        assert list(graph.network.nodes) == expected_nodes
+        assert list(graph.network.edges) == expected_edges
