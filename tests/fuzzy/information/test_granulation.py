f956e5e8a0bc0e97cecc7fd649d3404d36a70f60
johnHostetter <jhostetter16@gmail.com>
2023-01-24 16:34:05 -0500

    Fix test_add_edges; was not expecting multi-edge view

--- tests/fuzzy/information/test_granulation.py
+++ tests/fuzzy/information/test_granulation.py
@@ -15,7 +15,7 @@ class TestGranulation(unittest.TestCase):
             (1, 4), (1, 5), (1, 6), (1, 7), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)
         ]
         expected_edges = [
-            ((0, 1), (1, 1)), ((0, 7), (1, 3)), ((0, 7), (2, 5)), ((1, 2), (2, 3)), ((1, 3), (2, 5))
+            ((0, 1), (1, 1), 0), ((0, 7), (1, 3), 0), ((0, 7), (2, 5), 0), ((1, 2), (2, 3), 0), ((1, 3), (2, 5), 0)
         ]
         assert list(graph.network.nodes) == expected_nodes
         assert list(graph.network.edges) == expected_edges
