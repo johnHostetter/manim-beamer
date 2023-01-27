134c393ceb5ef3fc022e362dfae3d97e96537aa3
johnHostetter <jhostetter16@gmail.com>
2023-01-27 02:10:47 -0500

    Fix test for granulation; update requirements.txt

--- tests/fuzzy/information/test_granulation.py
+++ tests/fuzzy/information/test_granulation.py
@@ -15,13 +15,15 @@ class TestGranulation(unittest.TestCase):
         assert [vertex['id'] for vertex in graph.network.vs] == expected_nodes
 
         # each tuple is (variable index, term index)
-        graph.add(AlgebraicProduct, [((0, 1), (1, 1)), ((1, 2), (2, 3)), ((0, 7), (1, 3), (2, 5))])
+        edges = [((0, 1), (1, 1)), ((1, 2), (2, 3)), ((0, 7), (1, 3), (2, 5))]
+        graph.add(AlgebraicProduct, edges)
+
+        # only 3 AlgebraicProduct vertices should have been added to the graph; representing the fuzzy logic rule nodes
+        algebraic_product_vertices = graph.network.vs.select(relation_eq=AlgebraicProduct)
+        assert len(algebraic_product_vertices) == 3
 
         actual_edges = [
-            tuple(graph.network.vs[node]['id'] for node in graph.network.neighbors(edge.target))
-            for edge in graph.network.es
+            tuple(graph.network.vs[node]['id'] for node in graph.network.neighbors(rule_node))
+            for rule_node in algebraic_product_vertices
         ]
-        expected_edges = [
-            ((0, 1), (1, 1), 0), ((0, 7), (1, 3), 0), ((0, 7), (2, 5), 0), ((1, 2), (2, 3), 0), ((1, 3), (2, 5), 0)
-        ]
-        assert actual_edges == expected_edges
+        assert actual_edges == edges  # check that the edges have not changed after being added to the graph
