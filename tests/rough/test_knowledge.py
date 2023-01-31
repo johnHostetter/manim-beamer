3b29515681af558ed9c2f4b08efe51344af50f95
johnHostetter <jhostetter16@gmail.com>
2023-01-30 21:09:58 -0500

    Refactor RoughGranules for better organization

--- tests/rough/test_knowledge.py
+++ tests/rough/test_knowledge.py
@@ -91,3 +91,97 @@ class TestReductionOfKnowledge(unittest.TestCase):
 
     def test_core(self):
         assert self.gg.CORE({'P', 'Q', 'R'}) == frozenset({'P'})
+
+
+class TestRelativeReductAndRelativeCore(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.universe = frozenset(['x{}'.format(i) for i in range(1, 9)])
+        self.gg = GranulesGraph(self.universe)
+        self.gg.add('P', {frozenset({'x1', 'x3', 'x4', 'x5', 'x6', 'x7'}), frozenset({'x2', 'x8'})})
+        self.gg.add('Q', {frozenset({'x1', 'x3', 'x4', 'x5'}), frozenset({'x2', 'x6', 'x7', 'x8'})})
+        self.gg.add('R', {frozenset({'x1', 'x5', 'x6'}), frozenset({'x2', 'x7', 'x8'}), frozenset({'x3', 'x4'})})
+        self.gg.add('S', {
+            frozenset({'x1', 'x5', 'x6'}), frozenset({'x3', 'x4'}), frozenset({'x2', 'x7'}), frozenset({'x8'})})
+
+    def test_classification_with_all_relations(self):
+        # assert self.gg / {'P', 'Q', 'R'} == frozenset(
+        #     {frozenset({'x1', 'x5'}), frozenset({'x3', 'x4'}), frozenset({'x2', 'x8'}),
+        #      frozenset({'x6'}), frozenset({'x7'})
+        #      })
+        assert self.gg.IND({'P', 'Q', 'R'}) == frozenset(
+            {frozenset({'x1', 'x5'}), frozenset({'x3', 'x4'}), frozenset({'x2', 'x8'}),
+             frozenset({'x6'}), frozenset({'x7'})
+             })
+
+        assert self.gg.POS({'P', 'Q', 'R'}, {'S'}) == frozenset({'x1', 'x3', 'x4', 'x5', 'x6', 'x7'})
+
+    def test_P_is_S_indispensable(self):
+        equivalence_classes = self.gg.IND({'P', 'Q', 'R'} - {'P'})
+        assert equivalence_classes == frozenset(
+            {frozenset({'x1', 'x5'}), frozenset({'x3', 'x4'}), frozenset({'x2', 'x7', 'x8'}), frozenset({'x6'})})
+
+        assert self.gg.POS({'P', 'Q', 'R'} - {'P'}, {'S'}) == frozenset({'x1', 'x3', 'x4', 'x5', 'x6'})
+        assert self.gg.POS({'P', 'Q', 'R'} - {'P'}, {'S'}) != self.gg.POS({'P', 'Q', 'R'}, {'S'})
+        # hence, P is S-indispensible in {'P', 'Q', 'R'}
+
+    def test_Q_is_S_dispensable(self):
+        equivalence_classes = self.gg.IND({'P', 'Q', 'R'} - {'Q'})
+        assert equivalence_classes == frozenset(
+            {frozenset({'x1', 'x5', 'x6'}), frozenset({'x3', 'x4'}), frozenset({'x2', 'x8'}), frozenset({'x7'})})
+
+        assert self.gg.POS({'P', 'Q', 'R'} - {'Q'}, {'S'}) == frozenset({'x1', 'x3', 'x4', 'x5', 'x6', 'x7'})
+        assert self.gg.POS({'P', 'Q', 'R'} - {'Q'}, {'S'}) == self.gg.POS({'P', 'Q', 'R'}, {'S'})
+        # hence, Q is S-dispensible in {'P', 'Q', 'R'}
+
+    def test_R_is_S_indispensable(self):
+        equivalence_classes = self.gg.IND({'P', 'Q', 'R'} - {'R'})
+        assert equivalence_classes == frozenset(
+            {frozenset({'x1', 'x3', 'x4', 'x5'}), frozenset({'x2', 'x8'}), frozenset({'x6', 'x7'})})
+
+        assert self.gg.POS({'P', 'Q', 'R'} - {'R'}, {'S'}) == frozenset()
+        assert self.gg.POS({'P', 'Q', 'R'} - {'R'}, {'S'}) != self.gg.POS({'P', 'Q', 'R'}, {'S'})
+        # hence, R is S-indispensible in {'P', 'Q', 'R'}
+
+    def test_S_core(self):
+        assert frozenset({'P', 'R'})
+
+    def test_S_reduct(self):
+        assert frozenset({'P', 'R'})
+
+
+class TestReductionOfCategories(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.universe = frozenset(['x{}'.format(i) for i in range(1, 9)])
+        self.gg = GranulesGraph(self.universe)
+
+    def test_family_intersection(self):
+        self.gg.add('X', {frozenset({'x1', 'x3', 'x8'})})
+        self.gg.add('Y', {frozenset({'x1', 'x3', 'x4', 'x5', 'x6'})})
+        self.gg.add('Z', {frozenset({'x1', 'x3', 'x4', 'x6', 'x7'})})
+        # intersection of the family of sets
+        assert self.gg.family_intersection({'X', 'Y', 'Z'}) == frozenset({'x1', 'x3'})
+        # X is indispensable
+        assert self.gg.family_intersection({'X', 'Y', 'Z'} - {'X'}) == frozenset({'x1', 'x3', 'x4', 'x6'})
+        # Y is dispensable
+        assert self.gg.family_intersection({'X', 'Y', 'Z'} - {'Y'}) == frozenset({'x1', 'x3'})
+        # Z is dispensable
+        assert self.gg.family_intersection({'X', 'Y', 'Z'} - {'Z'}) == frozenset({'x1', 'x3'})
+
+    def test_family_union(self):
+        self.gg.add('X', {frozenset({'x1', 'x3', 'x8'})})
+        self.gg.add('Y', {frozenset({'x1', 'x2', 'x4', 'x5', 'x6'})})
+        self.gg.add('Z', {frozenset({'x1', 'x3', 'x4', 'x6', 'x7'})})
+        self.gg.add('T', {frozenset({'x1', 'x2', 'x5', 'x7'})})
+        # intersection of the family of sets
+        assert self.gg.family_union({'X', 'Y', 'Z', 'T'}) == self.universe
+        # X is indispensable
+        assert self.gg.family_union({'X', 'Y', 'Z', 'T'} - {'X'}) == frozenset(
+            {'x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7'})
+        # Y is dispensable
+        assert self.gg.family_union({'X', 'Y', 'Z', 'T'} - {'Y'}) == self.universe
+        # Z is dispensable
+        assert self.gg.family_union({'X', 'Y', 'Z', 'T'} - {'Z'}) == self.universe
+        # T is dispensable
+        assert self.gg.family_union({'X', 'Y', 'Z', 'T'} - {'T'}) == self.universe
