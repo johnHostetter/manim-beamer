98d8fcc7ba06bbeeb930722c77a7a26758db5ad9
johnHostetter <jhostetter16@gmail.com>
2023-02-02 15:15:36 -0500

    Move core knowledge base logic from RoughGraph to Core

--- tests/fuzzy/logic/control/test_tsk.py
+++ tests/fuzzy/logic/control/test_tsk.py
@@ -1,6 +1,16 @@
 import torch
 import unittest
 
+from utils.reproducibility import set_rng
+from soft.computing.graph import KnowledgeBase
+from soft.fuzzy.sets.continuous import Gaussian
+from soft.fuzzy.logic.control.tsk import ZeroOrderTSK
+from soft.fuzzy.relation.tnorm import AlgebraicProduct
+from soft.fuzzy.information.granulation import GranulesMap
+
+
+set_rng(0)
+
 
 class TestTSK(unittest.TestCase):
     def test_gradient_1(self):
@@ -32,3 +42,41 @@ class TestTSK(unittest.TestCase):
         b = torch.nn.Parameter(torch.tensor([1, 2]).float())
         c = 2 ** a
         assert c.grad_fn is not None
+
+    def test_tsk(self):
+        x = torch.tensor([[1.2, 0.2], [1.1, 0.3], [2.1, 0.1], [2.7, 0.15], [1.7, 0.25]]).float()
+        actual_y = torch.tensor([1.5, 0.6, 0.9, 0.7, 1.3]).float()
+
+        antecedents = [
+            Gaussian(4, centers=torch.tensor([1.2, 3.0, 5.0, 7.0]).float(),
+                     widths=torch.tensor([0.1, 0.4, 0.6, 0.8]).float()),
+            Gaussian(4, centers=torch.tensor([0.2, 0.6, 0.9, 1.2]).float(),
+                     widths=torch.tensor([0.4, 0.4, 0.5, 0.45]).float())
+        ]
+
+        # check that antecedents were correctly created
+        assert all(antecedents[0].centers == torch.tensor([1.2, 3.0, 5.0, 7.0]))
+        assert all(antecedents[0].widths == torch.tensor([0.1, 0.4, 0.6, 0.8]))
+        assert all(antecedents[1].centers == torch.tensor([0.2, 0.6, 0.9, 1.2]))
+        assert all(antecedents[1].widths == torch.tensor([0.4, 0.4, 0.5, 0.45]))
+
+        knowledge_base = GranulesMap(KnowledgeBase(antecedents))
+        knowledge_base.add(AlgebraicProduct, [((0, 0), (1, 0)), ((0, 1), (1, 0)), ((0, 1), (1, 1)), ((1, 1), (1, 1))])
+        rule_vertex = knowledge_base.graph.network.vs.find(relation_eq=AlgebraicProduct)
+        assert rule_vertex['relation'] == AlgebraicProduct  # it is the correct relation we wanted
+        assert 'id' in rule_vertex.attributes()  # it has a unique id
+        rule_vertices = knowledge_base.graph.network.vs.select(relation_eq=AlgebraicProduct)
+        assert len(rule_vertices) == 4  # there should be 4 fuzzy logic rules
+        # there should be 2 rules that use (1, 1); the last rule has been simplified (redundant mention of condition)
+        assert knowledge_base.graph[(1, 1)] == {AlgebraicProduct: [frozenset({(0, 1), (1, 1)}), frozenset({(1, 1)})]}
+
+        knowledge_base.graph.attributes(rule_vertex['id'])
+        n_output = actual_y.ndim
+        flc = ZeroOrderTSK(n_output, knowledge_base, input_trainable=True)
+        predicted_y = flc(x)
+        print(predicted_y)
+
+        print('sigmas: {}'.format(flc.input_granulation.sigmas))
+        print('log widths: {}'.format(flc.input_granulation._log_widths))
+        print('centers: {}'.format(flc.input_granulation.centers))
+        print('consequences: {}'.format(flc.consequences))
