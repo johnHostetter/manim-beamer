e13f29ff1ec2fd57ef84b5bd648a0a8b8f2710e2
johnHostetter <jhostetter16@gmail.com>
2023-02-03 17:25:27 -0500

    Fix test_granulation

--- tests/fuzzy/information/test_granulation.py
+++ tests/fuzzy/information/test_granulation.py
@@ -6,20 +6,29 @@ from soft.fuzzy.relation.tnorm import AlgebraicProduct
 
 
 class TestGranulation(unittest.TestCase):
-    def test_add_edges(self):
-        kb = KnowledgeBase(information=[Gaussian(8), Gaussian(4), Gaussian(6)])
-        expected_nodes = [
-            (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (1, 0), (1, 1), (1, 2), (1, 3),
-            (1, 4), (1, 5), (1, 6), (1, 7), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7)
-        ]
-        assert [vertex['id'] for vertex in kb.graph.vs] == expected_nodes
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.granules = [Gaussian(8), Gaussian(4), Gaussian(6)]
 
+    def test_add_granules(self):
+        kb = KnowledgeBase(information=None, config={'granules': self.granules})
+        kb.add_fuzzy_granules(self.granules)
+        expected_nodes = {
+            (0, 1), (0, 7), (2, 4), (1, 2), (0, 4), (2, 1), (0, 0), (1, 1), (0, 3),
+            (2, 0), (0, 6), (2, 3), (0, 2), (0, 5), (2, 2), (1, 0), (2, 5), (1, 3)
+        }
+
+        assert set([vertex['id'] for vertex in kb.graph.vs.select(variable_notin=[None])]) == expected_nodes
+
+    def test_add_rules(self):
+        kb = KnowledgeBase(information=None, config={'granules': self.granules})
         # each tuple is (variable index, term index)
-        edges = [((0, 1), (1, 1)), ((1, 2), (2, 3)), ((0, 7), (1, 3), (2, 5))]
+        edges = {
+            frozenset({(0, 1), (1, 1)}), frozenset({(1, 2), (2, 3)}), frozenset({(0, 7), (1, 3), (2, 5)})
+        }
         kb.add(AlgebraicProduct, edges)
 
         # only 3 AlgebraicProduct vertices should have been added to the graph; representing the fuzzy logic rule nodes
         algebraic_product_vertices = kb.graph.vs.select(relation_eq=AlgebraicProduct)
         assert len(algebraic_product_vertices) == 3
-
         assert kb.edges(AlgebraicProduct) == edges  # check the edges have not changed after being added to the graph
