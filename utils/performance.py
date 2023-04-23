a69e40d3d352104a858e3f018791de1d523632fb
johnHostetter <jhostetter16@gmail.com>
2023-04-23 18:01:14 -0400

    Remove unused libraries from requirements.txt

--- utils/performance.py
+++ utils/performance.py
@@ -4,19 +4,6 @@ certain features when the debugger is active.
 """
 import sys
 
-import ray
-
-
-# https://github.com/ray-project/ray/issues/10839
-def inside_tune():
-    """
-    Determine if the program session is inside of Ray Tune.
-
-    Returns:
-        True or False
-    """
-    return ray.tune.is_session_enabled()
-
 
 # https://stackoverflow.com/questions/38634988/check-if-program-runs-in-debug-mode
 def debugger_is_active() -> bool:
