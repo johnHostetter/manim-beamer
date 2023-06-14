54981ea214461d314f93c83b210486cbf8c620b1
johnHostetter <jhostetter16@gmail.com>
2023-06-14 15:07:30 -0400

    Update FCQL example to use d3rlpy implementation

--- utils/performance.py
+++ utils/performance.py
@@ -4,6 +4,8 @@ certain features when the debugger is active.
 """
 import sys
 
+import torch
+
 
 # https://stackoverflow.com/questions/38634988/check-if-program-runs-in-debug-mode
 def debugger_is_active() -> bool:
@@ -14,3 +16,19 @@ def debugger_is_active() -> bool:
         True or False
     """
     return hasattr(sys, "gettrace") and sys.gettrace() is not None
+
+
+def performance_boost() -> bool:
+    """
+    If the debugger is not active, improve the performance of Torch by disabling some features.
+
+    Returns:
+        True if the performance was boosted, False otherwise.
+    """
+    # for performance boost, disable the following when we are not going to debug:
+    toggle_performance_boost = not debugger_is_active()
+    if toggle_performance_boost:
+        torch.autograd.set_detect_anomaly(debugger_is_active())
+        torch.autograd.profiler.profile(debugger_is_active())
+        torch.autograd.profiler.emit_nvtx(debugger_is_active())
+    return toggle_performance_boost
