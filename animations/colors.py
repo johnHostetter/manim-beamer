8da9c99a97f89cb13e454ccacb427bf5b6bee8eb
John Wesley Hostetter <jhostetter16@gmail.com>
2024-03-06 23:47:22 -0500

    Change colors to Enum

--- animations/colors.py
+++
@@ -1,5 +0,0 @@
-ACTIVE_ITEM_1 = "#FD56DC"  # hot pink
-INACTIVE_ITEM_1 = "#FFB9CB"  # light pink
-ACTIVE_ITEM_2 = "#68EF00"  # hot pink
-INACTIVE_ITEM_2 = "#01D3FC"  # light pink
-BACKGROUND_ITEM = "#025393"  # dark blue
