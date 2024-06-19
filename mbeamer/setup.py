7a1a82090d0e032932a17bb210673c44a5ad8382
John Wesley Hostetter <jhostetter16@gmail.com>
2024-06-19 12:54:27 -0400

    Modify setup

---
+++ mbeamer/setup.py
@@ -0,0 +1,12 @@
+"""
+Set up the 'soft' computing Python library.
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="mbeamer",
+    version="1.0",
+    author="John Wesley Hostetter",
+    author_email="jhostetter16@gmail.com",
+    packages=["mbeamer"],
+)
